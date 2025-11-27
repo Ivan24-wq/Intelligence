b3ecbdd42ec9e6c1ab3d330c5ffd0ec4de5c63a4
Ivan24-wq <iv8747583@gmail.com>
2025-11-27 16:48:52 +0300

    Task2

--- Practical work №2/main.py
+++ Practical work №2/main.py
@@ -1,67 +1,84 @@
-# Ваш код для Задания 1
-#Импорт библиотек
+# Ваш код для Задания 2
+#Библиотеки
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Perceptron
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.decomposition import PCA
 import numpy as np
+from sklearn.linear_model import Perceptron
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
 
-# 1. Многоклассовая классификация с помощью встроенного метода
-CSV_FILE = "train.csv"
-df = pd.read_csv(CSV_FILE)
-#Целевая переменная
-target_col = "Survived"
 
-#Удаляем ненужные столбцы
+#Код
+CSV_FILE = ("train.csv")
+df = pd.read_csv(CSV_FILE)  #Загружаем датасет
+
+#Для новых классов
+target = "Pclass"
+
+#Удаление ненужных колонок
 df = df.drop(columns=["PassengerId", "Name", "Ticket", "Cabin"])
 
-#разделение признаков
-X = df.drop(columns=[target_col])
-y = df[target_col]
+#Кодировка
+df = pd.get_dummies(df)
+df = df.fillna(df.mean())
+X = df.drop(columns=[target])
+y = df[target]
 
-#Кодировка текстовых признаков
-X = pd.get_dummies(X)
-#Заполнение пропусков
-X = X.fillna(X.mean())
+#2D Визуализация
+pca = PCA(n_components=2)
+X2 = pca.fit_transform(X)
 
-#Обучающая выборка
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size = 0.3, random_state = 51, stratify = y   #Разделение в соотношении 70 на обучение, 30 на тест
-)
+#Обучение пар классов
+def analyze(class_a, class_b, pair_id):
+    #Выборка 2 классов
+    mask = (y == class_a) | (y == class_b)
+    Xa = X2[mask]
+    ya = y[mask]
+    
+    #Перекодировка
+    ya_bin = np.where(ya == class_a, 1, -1)
+    #Обучение перцептрона
+    clf = Perceptron(max_iter=2000, random_state=51)
+    clf.fit(Xa, ya_bin)
+    y_pred = clf.predict(Xa)
+    acc = accuracy_score(ya_bin, y_pred)
+    
+    #График
+    plt.figure(figsize=(6, 5))
+    plt.scatter(Xa[:, 0], Xa[:, 1], c = ya_bin, cmap="coolwarm", alpha=0.6)
+    w = clf.coef_[0]
+    b = clf.intercept_[0]
+    
+    #Построение прямой
+    x_line = np.linspace(Xa[:, 0].min(), Xa[:, 0].max(), 100)
+    y_line = -(w[0] * x_line + b) / w[1]
+    
+    #График
+    plt.plot(x_line, y_line, "k--", label = "Граница")
+    plt.title(f"Пара кластеров {class_a} vs {class_b}\nТочность: ")
+    plt.xlabel("PCA1")
+    plt.ylabel("PCA2")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    
+    print(f"Пара кластеров {class_a} vs {class_b} === ")
+    print(f"Точность: {acc:.4f}")
+    print()
+    
+    return acc
 
-clf_mc = Perceptron(max_iter=1000, random_state=51)
-clf_mc.fit(X_train, y_train)
+# 1. 1 класс vs 2 класс
+acc12 = analyze(1, 2, 1)
 
-y_pred_mc = clf_mc.predict(X_test)
+# 2. 1 класс vs 3 класс
+acc13 = analyze(1, 3, 2)
 
-print("\n Встроенный Перцептрон")
-print(f"Точность: {accuracy_score(y_test, y_pred_mc)}")
-print(classification_report(y_test, y_pred_mc))
-# 2. Подход "один против всех" (One-vs-Rest)
-unique_classes = np.unique(y_train)
-binary_classifier = {}
+# 3. 2 класс vs 3 класс
+acc23 = analyze(2, 3, 3)
 
-#Бинарные метки
-for cls in unique_classes:
-    y_train_bin = np.where(y_train == cls, 1, -1)
-    clf = Perceptron(max_iter=1000, random_state=51)
-    clf.fit(X_train, y_train_bin)
-    binary_classifier[cls] = clf
-
-#Предсказание Ovr
-def predict_ovr(X):
-    scores = []
-    for cls, clf in binary_classifier.items():
-        score = X.values @ clf.coef_.T + clf.intercept_
-        scores.append(score)
-    scores = np.hstack(scores)      # превращаем список массивов в матрицу N x C
-    return np.argmax(scores, axis=1)
-
-
-y_pred_ovr = predict_ovr(X_test)
-
-# 3. Сравнение результатов
-print("\n Бинарный перцептрон")
-print(f"Точность: {accuracy_score(y_test, y_pred_ovr)}")
-print(classification_report(y_test, y_pred_ovr))
+# 4. Анализ результатов
+print("Итоговый анализ")
+print(f"1 vs 2: {acc12:.4f}")
+print(f" vs 3: {acc13:.4f}")
+print(f"2 vs 3: {acc23:.4f}")
